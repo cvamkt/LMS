11d5c550a457debf3dfed2e540288416fd89d205
shivam kumar <154726545+cvamkt@users.noreply.github.com>
2024-09-11 01:44:34 +0530

    Update axiosInstance.js

--- lms-fronted/src/helpers/axiosInstance.js
+++ lms-fronted/src/helpers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // https://learning-management-system-b1g5.onrender.com
 // http://localhost:5000/api/v1
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://lms-qw20.onrender.com/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
