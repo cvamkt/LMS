19219a516b75d1389e3b0b9bb4898f6b2493d2ca
shivam kumar <shivamkrthakur9771@gmail.com>
2024-09-11 04:19:27 +0530

    added react_icons link

--- lms-fronted/src/Components/footer.jsx
+++ lms-fronted/src/Components/footer.jsx
@@ -1,4 +1,4 @@
-import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
+import {BsFacebook, BsGithub, BsInstagram, BsLinkedin} from "react-icons/bs";
 
 function Footer() {
     const currentDate = new Date();
@@ -10,17 +10,33 @@ function Footer() {
                     Copyright {year}  | All right reserved
                 </section>
                 <section className="flex items-center justify-center gap-5 text-2xl text-white sm: mt-2">
-                    <a className="hover:text-yellow-500 transition-all ease-in-out duration-300">
+                    <a
+                        href="https://www.facebook.com/pwskills"
+                        target=  "_blank"
+                        rel="noopener noreferrer"
+                        className="hover:text-yellow-500 transition-all ease-in-out duration-300">
                         <BsFacebook />
                     </a>
-                    <a className="hover:text-yellow-500 transition-all ease-in-out duration-300">
+                    <a
+                        href="https://www.instagram.com/isanketsingh"
+                        target=  "_blank"
+                        rel="noopener noreferrer"
+                        className="hover:text-yellow-500 transition-all ease-in-out duration-300">
                         <BsInstagram />
                     </a>
-                    <a className="hover:text-yellow-500 transition-all ease-in-out duration-300">
+                    <a
+                        href="https://www.linkedin.com/in/shivam-kumar-5815b01a3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                        target=  "_blank"
+                        rel="noopener noreferrer"
+                        className="hover:text-yellow-500 transition-all ease-in-out duration-300">
                         <BsLinkedin />
                     </a>
-                    <a className="hover:text-yellow-500 transition-all ease-in-out duration-300">
-                        <BsTwitter />
+                    <a
+                        href="https://github.com/cvamkt"
+                        target=  "_blank"
+                        rel="noopener noreferrer"
+                        className="hover:text-yellow-500 transition-all ease-in-out duration-300">
+                        <BsGithub />
                     </a>
                 </section>
             </footer>
@@ -29,3 +45,8 @@ function Footer() {
 }
 
 export default Footer;
+
+
+
+
+
